d4d071642596d121b10aea6d4e80edc06c803d3c
balutarock <balutarock71117@gmail.com>
2022-07-03 12:01:58 +0530

    first commit

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Component } from "react";
 import {
   Text,
   Link,
@@ -11,6 +12,11 @@ import {
   extendTheme,
   VStack,
   Box,
+  Flex,
+  Input,
+  Image,
+  Progress,
+  Button,
 } from "native-base";
 import NativeBaseIcon from "./components/NativeBaseIcon";
 import { Platform } from "react-native";
@@ -24,62 +30,240 @@ const config = {
 // extend the theme
 export const theme = extendTheme({ config });
 
-export default function App() {
-  return (
-    <NativeBaseProvider>
-      <Center
-        _dark={{ bg: "blueGray.900" }}
-        _light={{ bg: "blueGray.50" }}
-        px={4}
-        flex={1}
-      >
-        <VStack space={5} alignItems="center">
-          <NativeBaseIcon />
-          <Heading size="lg">Welcome to NativeBase</Heading>
-          <HStack space={2} alignItems="center">
-            <Text>Edit</Text>
-            <Box
-              _web={{
-                _text: {
-                  fontFamily: "monospace",
-                  fontSize: "sm",
-                },
-              }}
-              px={2}
-              py={1}
-              _dark={{ bg: "blueGray.800" }}
-              _light={{ bg: "blueGray.200" }}
-            >
-              App.js
-            </Box>
-            <Text>and save to reload.</Text>
-          </HStack>
-          <Link href="https://docs.nativebase.io" isExternal>
-            <Text color="primary.500" underline fontSize={"xl"}>
-              Learn NativeBase
-            </Text>
-          </Link>
-          <ToggleDarkMode />
-        </VStack>
-      </Center>
-    </NativeBaseProvider>
+const apiStatusListOfFirst = {
+  initial: "INITIAL",
+  in_progress: "IN_PROGRESS",
+  success: "SUCCESS",
+  failure: "FAILURE",
+};
+
+// const moviesData = [];
+
+// export default function App() {
+class App extends Component {
+  state = {
+    apiStatusOfFirst: apiStatusListOfFirst.initial,
+    moviesData: [],
+    search: "",
+  };
+
+  componentDidMount() {
+    this.getTheMovieData();
+  }
+
+  getTheMovieData = async () => {
+    this.setState({ apiStatusOfFirst: apiStatusListOfFirst.in_progress });
+    console.log("balu");
+    const originalApiUrl =
+      "https://api.themoviedb.org/3/discover/tv?api_key=639ba2e19fa297642eec1cefb28ef177";
+
+    // ('https://api.themoviedb.org/3/discover/tv?api_key=639ba2e19fa297642eec1cefb28ef177') //
+    // 'https://api.themoviedb.org/3/movie/popular?api_key=639ba2e19fa297642eec1cefb28ef177&language=en-US&' //
+
+    const options = {
+      method: "GET",
+    };
+    const response = await fetch(originalApiUrl, options);
+    if (response.ok === true) {
+      const data = await response.json();
+      console.log("data >>> ", data);
+      // const arrayLength = data.results.length;
+      // const randomItem =
+      //   data.results[Math.floor(Math.random() * (arrayLength - 1))];
+      // const updatedData = {
+      //   id: randomItem.id,
+      //   title: randomItem.name,
+      //   backdropPath: randomItem.backdrop_path,
+      //   overview: randomItem.overview,
+      // };
+      this.setState({
+        moviesData: [...data.results],
+        apiStatusOfFirst: apiStatusListOfFirst.success,
+      });
+      // return data.results;
+    } else {
+      this.setState({ apiStatusOfFirst: apiStatusListOfFirst.failure });
+    }
+  };
+
+  renderTheNoResults = () => (
+    <Flex
+      direction="column"
+      w="100%"
+      justifyContent="center"
+      align="center"
+      h="500"
+    >
+      <Image
+        source={{
+          uri: "https://res.cloudinary.com/dxnhvq8pl/image/upload/v1643303783/movie%20app%20mini%20project/alert-triangle_eeuzgc.png",
+        }}
+        alt="failure view"
+        w="50"
+        h="50"
+      />
+      <Text fontWeight="bold">No serach results</Text>
+    </Flex>
   );
+
+  renderTheSuccess = () => {
+    const { moviesData, search } = this.state;
+    const filteredData = moviesData.filter((each) =>
+      each.name.toLowerCase().includes(search.toLowerCase())
+    );
+
+    return (
+      <>
+        {filteredData.length == 0 ? (
+          this.renderTheNoResults()
+        ) : (
+          <Flex
+            direction="row"
+            flexWrap="wrap"
+            gap="10"
+            w="100%"
+            justify="center"
+          >
+            {filteredData.map((each) => {
+              console.log("  ", each.poster_path);
+              return (
+                <Box key={each.id} shadow={"9"} w={["270", "300"]} rounded="5">
+                  <Image
+                    source={{
+                      uri: `https://image.tmdb.org/t/p/original${each.poster_path}`,
+                    }}
+                    alt="Alternate Text"
+                    h="400"
+                    w="300"
+                  />
+                  <Center>
+                    <Text
+                      fontFamily="Bree serif"
+                      fontWeight={600}
+                      fontSize="25"
+                    >
+                      {each.name}
+                    </Text>
+                  </Center>
+                </Box>
+              );
+            })}
+          </Flex>
+        )}
+      </>
+    );
+  };
+
+  renderTheFailure = () => (
+    <Box w="100%" h="500">
+      <Flex direction="column" justifyContent="center" align="center" h="500">
+        <Image
+          source={{
+            uri: "https://res.cloudinary.com/dxnhvq8pl/image/upload/v1643303783/movie%20app%20mini%20project/alert-triangle_eeuzgc.png",
+          }}
+          alt="failure view"
+          w="50"
+          h="50"
+        />
+        <Text fontWeight="light">Something went wrong. Please try again</Text>
+        <Button small primary>
+          <Text fontWeight="bold">Try Again</Text>
+        </Button>
+      </Flex>
+    </Box>
+  );
+  renderTheLoader = () => (
+    <Box w="100%" h="500">
+      <Flex direction="row" justify="center" align="center" h="500">
+        <Progress w="300" shadow={2} value={45} mx="4" />
+      </Flex>
+    </Box>
+  );
+
+  renderTheHomePageStatus = () => {
+    const { apiStatusOfFirst } = this.state;
+    switch (apiStatusOfFirst) {
+      case apiStatusListOfFirst.success:
+        return this.renderTheSuccess();
+      case apiStatusListOfFirst.failure:
+        return this.renderTheFailure();
+      case apiStatusListOfFirst.in_progress:
+        return this.renderTheLoader();
+
+      default:
+        return null;
+    }
+  };
+
+  onChangeSearch = (event) => {
+    console.log("onChange", event.target.value);
+    this.setState({
+      search: event.target.value,
+    });
+  };
+
+  render() {
+    const { moviesData, search } = this.state;
+    console.log("mvo >>> ", moviesData);
+    console.log("search ", search);
+    return (
+      <NativeBaseProvider>
+        <VStack space={5} mb="50" positon="fixed-top">
+          <Box w="100%" py="10" px="10" shadow={"4"}>
+            <Flex
+              direction="row"
+              justifyContent="start"
+              align="center"
+              w="400px"
+            >
+              <Flex direction="row" align="center" pr="10">
+                <NativeBaseIcon fontSize="md" />
+                <Heading size="lg" fontSize={["20", "30"]}>
+                  NativeBase
+                </Heading>
+              </Flex>
+              <Flex direction="row">
+                <Text pr="4">Home</Text>
+                <Text>production</Text>
+              </Flex>
+              <Input
+                mx="3"
+                placeholder="Search"
+                w={["100", "200", "300"]}
+                onChange={this.onChangeSearch}
+              />
+            </Flex>
+          </Box>
+        </VStack>
+        <Box pl="20" pr="20">
+          {this.renderTheHomePageStatus()}
+        </Box>
+      </NativeBaseProvider>
+    );
+  }
 }
 
-// Color Switch Component
-function ToggleDarkMode() {
-  const { colorMode, toggleColorMode } = useColorMode();
-  return (
-    <HStack space={2} alignItems="center">
-      <Text>Dark</Text>
-      <Switch
-        isChecked={colorMode === "light"}
-        onToggle={toggleColorMode}
-        aria-label={
-          colorMode === "light" ? "switch to dark mode" : "switch to light mode"
-        }
-      />
-      <Text>Light</Text>
-    </HStack>
-  );
-}
+export default App;
+
+// const renderThePage = () => {
+//   setData = [getMoviesData()];
+// };
+
+// getMoviesData();
+// // Color Switch Component
+// function ToggleDarkMode() {
+//   const { colorMode, toggleColorMode } = useColorMode();
+//   return (
+//     <HStack space={2} alignItems="center">
+//       <Text>Dark</Text>
+//       <Switch
+//         isChecked={colorMode === "light"}
+//         onToggle={toggleColorMode}
+//         aria-label={
+//           colorMode === "light" ? "switch to dark mode" : "switch to light mode"
+//         }
+//       />
+//       <Text>Light</Text>
+//     </HStack>
+//   );
+// }
--- Readme.md
+++ Readme.md
@@ -7,3 +7,72 @@ The official NativeBase template for [Expo](https://docs.expo.io/)
 ```sh
 expo init my-app --template @native-base/expo-template
 ```
+
+# Bala Nagendra kumar
+
+## Web, Android, ios support system --> NativeBase
+
+[![N|Solid](https://res.cloudinary.com/dxnhvq8pl/image/upload/v1656827930/movie%20app%20mini%20project/NativebaseLogo_zo5r6r.png)](https://nativebase.io/)
+
+[![Build Status](https://res.cloudinary.com/dxnhvq8pl/image/upload/v1656827444/movie%20app%20mini%20project/Screenshot_from_2022-07-03_11-15-44_sn6vj4.png)](https://res.cloudinary.com/dxnhvq8pl/image/upload/v1656827444/movie%20app%20mini%20project/Screenshot_from_2022-07-03_11-15-44_sn6vj4.png)
+
+OTT is a streaming platform, mobile-responsive, web responsive.
+
+- Type some Markdown on the left
+- See HTML in the right
+- ✨Magic ✨
+
+## Features
+
+- The app would shows the movies list.
+- We can search the movie based on the movie title.
+- Handling Errors, while getting the movie data, incase any error got the app, the app would handle the errors.
+- It will display the Loader while fetching the data.
+- Filter feature is available, based on the movie name, we can search.
+- while search the matched data will display otherwise it will show the No data found.
+
+The application would support in Web, Android, Ios platforms
+that people see the plation for Entertainment.
+As [Balu Nagendra Kumar]
+
+This text you see here is \*actually- written in React by using NativeBase expo platform!
+
+## Tech
+
+OTT is streaming platform:
+
+- [ReactJS] - JSX,JS!
+- [NativeBase] - awesome web-based platform.
+
+- [Breakdance](https://breakdance.github.io/breakdance/) - ReactJS, NativeBase
+
+## Installation
+
+Install the dependencies and devDependencies and start the server.
+
+npm i
+npm start (or) expo start
+
+````
+
+For production environments...
+
+```sh
+npm install --production
+NODE_ENV=production node app
+````
+
+## Plugins
+
+| Plugin           | README                                    |
+| ---------------- | ----------------------------------------- |
+| Dropbox          | [plugins/dropbox/README.md][pldb]         |
+| GitHub           | [plugins/github/README.md][plgh]          |
+| Google Drive     | [plugins/googledrive/README.md][plgd]     |
+| OneDrive         | [plugins/onedrive/README.md][plod]        |
+| Medium           | [plugins/medium/README.md][plme]          |
+| Google Analytics | [plugins/googleanalytics/README.md][plga] |
+
+## Development
+
+Want to contribute? Great!
